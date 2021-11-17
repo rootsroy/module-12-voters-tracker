5c18511c231778912f0780bbf813f6b757310ba3
Ruth Torres <ruths.roy@gmail.com>
2021-11-16 22:42:53 -0800

    added paryies table

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.get("/", (req, res) => {
 
 // GET all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates
+        LEFT JOIN parties ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -42,7 +43,132 @@ app.get("/api/candidates", (req, res) => {
 
 // GET a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates
+        LEFT JOIN parties ON candidates.party_id = parties.id
+        WHERE candidates.id = ?`;
+  const params = [req.params.id];
+
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+// DELETE a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({
+      message: "successfully deleted",
+      changes: this.changes,
+    });
+  });
+});
+
+// CREATE a candidate
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?, ?, ?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
+    });
+  });
+});
+
+// default response for any other requests -- catch all
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+db.on("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+  });
+});
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const sqlite3 = require("sqlite3").verbose();
+const inputCheck = require("./utils/inputCheck");
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to database
+const db = new sqlite3.Database("./db/election.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log("Connected to the election database.");
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+// GET all candidates
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates
+        LEFT JOIN parties ON candidates.party_id = parties.id`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+// GET a single candidate
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates
+        LEFT JOIN parties ON candidates.party_id = parties.id
+        WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.get(sql, params, (err, row) => {
