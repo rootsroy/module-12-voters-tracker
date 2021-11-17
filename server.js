47aee75c25344c6e306d9a6a899a447dad811d73
Ruth Torres <ruths.roy@gmail.com>
2021-11-16 22:38:00 -0800

    added post route and tables

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sqlite3 = require("sqlite3").verbose();
+const inputCheck = require("./utils/inputCheck");
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -76,16 +77,36 @@ app.delete("/api/candidate/:id", (req, res) => {
 });
 
 // CREATE a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-// VALUES (?, ?, ?, ?)`;
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
 
-// const params = [1, 'Ronald', 'Firbank', 1];
-// db.run(sql,params, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     };
-//     console.log(result, this.lastID);
-// });
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?, ?, ?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
+    });
+  });
+});
 
 // default response for any other requests -- catch all
 app.use((req, res) => {
